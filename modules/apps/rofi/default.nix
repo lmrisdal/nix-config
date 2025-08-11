eb9de6dbca4820dbf8c66f706a6c937504da3316
Lars Risdal <larsrisdal@gmail.com>
2025-08-11 15:18:54 +0200

    add rofi theme

--- modules/apps/rofi/default.nix
+++ modules/apps/rofi/default.nix
@@ -21,11 +21,20 @@ in
     home-manager.users.${username} =
       { pkgs, config, ... }:
       {
+        home.file = {
+          "${config.xdg.configHome}/rofi/themes/spotlight-dark.rasi" = {
+            source = pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/newmanls/rofi-themes-collection/refs/heads/master/themes/spotlight-dark.rasi";
+              sha256 = "sha256-z+ZM0oTo2Lym0a4xReAPArdQKtpwPk9FDMnuCh57ZMI=";
+            };
+          };
+        };
         programs.rofi = {
           enable = true;
           package = pkgs.rofi-wayland;
           location = "center";
-          theme = "arthur";
+          theme = "${config.xdg.configHome}/rofi/themes/spotlight-dark.rasi";
+          terminal = "kitty";
           plugins = with pkgs; [
             rofi-emoji
             rofi-calc
