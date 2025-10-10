0bf14910317cfe3f5b8ddc934d2c718ed02d5fec
Lars Risdal <larsrisdal@gmail.com>
2025-10-10 11:18:51 +0200

    add raycast-like hyper key

--- modules/apps/vicinae/default.nix
+++ modules/apps/vicinae/default.nix
@@ -15,10 +15,36 @@ in
     };
   };
   config = lib.mkIf cfg.enable {
+    # raycast like hyper key
+    services.interception-tools = {
+      enable = true;
+      plugins = [ pkgs.interception-tools-plugins.dual-function-keys ];
+      udevmonConfig = ''
+        - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c /home/${username}/.config/interception/dual-function-keys.yaml | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK]
+      '';
+    };
     home-manager.users.${username} = {
       services.vicinae = {
-        enable = true; # default: true
-        autoStart = true; # default: true
+        enable = true;
+        autoStart = true;
+      };
+      home.file = {
+        ".config/interception/dual-function-keys.yaml".text = ''
+          TIMING:
+            TAP_MILLISEC: 50
+
+          MAPPINGS:
+            - KEY: KEY_CAPSLOCK
+              TAP: KEY_CAPSLOCK
+              HOLD:
+                - KEY_LEFTCTRL
+                - KEY_LEFTALT
+                - KEY_LEFTMETA
+                - KEY_LEFTSHIFT
+        '';
       };
     };
   };
